fca588378ed70a5e8b579c352a9cae9d2f41d64e
tkm3d1a <tim.klimpel@gmail.com>
2022-06-05 10:46:15 -0400

    Testing driver class utilization - success

--- src/GUIDriver.java
+++ src/GUIDriver.java
@@ -1,5 +1,10 @@
 public class GUIDriver {
     public static void main(String[] args) throws Exception {
+
+        GUIDialog dialog = new GUIDialog();
+        dialog.start();
+
         System.out.println("Hello, World!");
+        
     }
 }
